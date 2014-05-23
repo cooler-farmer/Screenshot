3cb0bbc941467ba72fcf21c1386e3437ffdc2dc5
ByteBlast <byteblast@gmail.com>
2014-05-23 08:51:06 +0100

    added screenshot provider.

--- Screenshot/src/Screenshot/Screenshot/Forms/MainForm.Designer.cs
+++ Screenshot/src/Screenshot/Screenshot/Forms/MainForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Screenshot
+﻿namespace Screenshot.Forms
 {
     partial class MainForm
     {
@@ -28,12 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonTakeScreenshot = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonTakeScreenshot
+            // 
+            this.buttonTakeScreenshot.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTakeScreenshot.Location = new System.Drawing.Point(12, 12);
+            this.buttonTakeScreenshot.Name = "buttonTakeScreenshot";
+            this.buttonTakeScreenshot.Size = new System.Drawing.Size(153, 31);
+            this.buttonTakeScreenshot.TabIndex = 0;
+            this.buttonTakeScreenshot.Text = "Take Screenshot";
+            this.buttonTakeScreenshot.UseVisualStyleBackColor = true;
+            this.buttonTakeScreenshot.Click += new System.EventHandler(this.buttonTakeScreenshot_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 49);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(694, 426);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(718, 487);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.buttonTakeScreenshot);
+            this.Name = "MainForm";
             this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonTakeScreenshot;
+        private System.Windows.Forms.PictureBox pictureBox;
     }
 }
 
--- Screenshot/src/Screenshot/Screenshot/Forms/MainForm.cs
+++ Screenshot/src/Screenshot/Screenshot/Forms/MainForm.cs
@@ -1,20 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Screenshot
+namespace Screenshot.Forms
 {
     public partial class MainForm : Form
     {
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void buttonTakeScreenshot_Click(object sender, EventArgs e)
+        {
+            pictureBox.Image = 
+                ScreenshotProvider.TakeScreenshot();
         }
     }
 }
