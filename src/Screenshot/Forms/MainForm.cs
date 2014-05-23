39f97b62e7dd249fb790b4d35c6f11d3ceb8eca6
r4wr00 <nick.rawr0@gmail.com>
2014-05-23 05:27:18 -0400

    Added Byte's Hotkey Library

--- src/Screenshot/Forms/MainForm.cs
+++ src/Screenshot/Forms/MainForm.cs
@@ -1,18 +1,32 @@
 ﻿using System;
 using System.Windows.Forms;
+using Shortcut;
 
 namespace Screenshot.Forms
 {
     public partial class MainForm : Form
     {
+        // Binder for ye Hotkeys (Sexy library Byte!)
+        private readonly HotkeyBinder _hotkeyBinder = new HotkeyBinder();
+        
         public MainForm()
         {
             InitializeComponent();
+
+            var hotKeyPrintScrn = new HotkeyCombination(Modifiers.None, Keys.PrintScreen);
+            _hotkeyBinder.Bind(hotKeyPrintScrn).To(this.HotKeyCallBackPrintScreen);
         }
 
+        private void HotKeyCallBackPrintScreen()
+        {
+            pictureBox.Image =
+                ScreenshotProvider.TakeScreenshot();
+        }
+        
         private void buttonTakeScreenshot_Click(object sender, EventArgs e)
         {
-            pictureBox.Image = 
+            // nice and easy for anybody to use ^^ - couldnt be simpler ;) -Nick
+            pictureBox.Image =
                 ScreenshotProvider.TakeScreenshot();
         }
     }
